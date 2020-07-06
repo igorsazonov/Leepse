4c91ffa6cea47e70b212892fed683319c0d2429c
igorsazonov <sazonovkerch@gmail.com>
2020-07-06 13:27:54 +0300

    Initial Commit

---
+++ Leepse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leepse
+//
+//  Created by Игорь Сазонов on 06.07.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
