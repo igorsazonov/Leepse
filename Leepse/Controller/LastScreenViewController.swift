7268eb9c70f3fbc5a346b6cf1d064674d30bce64
igorsazonov <sazonovkerch@gmail.com>
2020-07-07 19:52:53 +0300

    changes 07.07

---
+++ Leepse/Controller/LastScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LastScreenViewController.swift
+//  Leepse
+//
+//  Created by Игорь Сазонов on 07.07.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class LastScreenViewController: UIViewController {
+
+    @IBAction func logoutInApp(_ sender: Any) {
+        
+    }
+    
+    @IBAction func logoutInAp(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
