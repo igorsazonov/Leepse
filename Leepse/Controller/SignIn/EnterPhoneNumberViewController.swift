241251146c94ec0d89d80fa21ab120651c5d4abd
igorsazonov <sazonovkerch@gmail.com>
2020-07-07 23:47:10 +0300

    changes 07.07

---
+++ Leepse/Controller/SignIn/EnterPhoneNumberViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EnterPhoneNumberViewController.swift
+//  Leepse
+//
+//  Created by Игорь Сазонов on 07.07.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class EnterPhoneNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
