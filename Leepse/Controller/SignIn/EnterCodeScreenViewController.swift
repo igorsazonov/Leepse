cf8974dd6a56a94f801ec438840b6fcc41e69fa0
igorsazonov <sazonovkerch@gmail.com>
2020-07-08 06:42:14 +0300

    changes 07.07

---
+++ Leepse/Controller/SignIn/EnterCodeScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EnterCodeScreenViewController.swift
+//  Leepse
+//
+//  Created by Игорь Сазонов on 08.07.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class EnterCodeScreenViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Leepse/Controller/SignIn/EnterPhoneNumberViewController.swift
+++ Leepse/Controller/SignIn/EnterPhoneNumberViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class EnterPhoneNumberViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func backToFirstScreen(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func goToEnterCodeScreen(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingUpTheView()
+    }
+    
+    func settingUpTheView() {
+        messageLabel.text = "Enter the phone number associated with your username"
         
+        submitButton.layer.cornerRadius = 16.7
+        submitButton.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        submitButton.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.7176470588, blue: 0.3921568627, alpha: 1)
     }
 }
