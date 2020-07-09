e16f9165b2414602e66c57a4e348341dc1749f8e
igorsazonov <sazonovkerch@gmail.com>
2020-07-09 07:30:31 +0300

    changes 08.07

--- Leepse/Controller/SignIn/EnterPhoneNumberViewController.swift
+++ Leepse/Controller/SignIn/EnterPhoneNumberViewController.swift
@@ -15,21 +15,24 @@ class EnterPhoneNumberViewController: UIViewController {
     @IBOutlet weak var submitButton: UIButton!
     
     @IBAction func tappedSubmit(_ sender: UIButton) {
-         
+        let enterCodeScreenViewController =  UIStoryboard(name: "SignIn", bundle: Bundle.main).instantiateViewController(withIdentifier: "EnterCodeVc") as! EnterCodeScreenViewController
+        navigationController?.pushViewController(enterCodeScreenViewController, animated: true)
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       // configuringTheButton()
+        configuringTheButton()
         configuringTheLabel()
     }
-    /*
+    
     func configuringTheButton() {
-        submitButton.layer.cornerRadius = 15.7
+        submitButton.layer.cornerRadius = 16.7
         submitButton.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         submitButton.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.7176470588, blue: 0.3921568627, alpha: 1)
     }
-    */
+    
     func configuringTheLabel() {
         messageLabel.text = "Enter the phone number associated with your username"
     }
