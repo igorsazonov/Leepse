f67730cc95d7acce923024a46f51e9d7db8b4390
igorsazonov <sazonovkerch@gmail.com>
2020-07-09 20:24:50 +0300

    changes 09.07

--- Leepse/Controller/SignUp/EnterCodeSignUpViewController.swift
+++ Leepse/Controller/SignUp/EnterCodeSignUpViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 
 class EnterCodeSignUpViewController: UIViewController {
 
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func tappedSubmit(_ sender: UIButton) {
+        transitionToViewController()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        customLabelAndButton()
+        codeTextField.defaultTextAttributes.updateValue(50.0, forKey: NSAttributedString.Key.kern)
+    }
+    
+    func customLabelAndButton() {
+        numberLabel.text = ""
+        messageLabel.text = "message"
+        submitButton.layer.cornerRadius = 16.7
+        submitButton.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        submitButton.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.7176470588, blue: 0.3921568627, alpha: 1)
+    }
+    
+    func transitionToViewController() {
+        let lastScreenViewController = UIStoryboard(name: "Last", bundle: .main).instantiateViewController(withIdentifier: "lastVc") as! LastScreenViewController
+        navigationController?.setViewControllers([lastScreenViewController], animated: true)
     }
 }
--- Leepse/Controller/SignUp/UserNameViewController.swift
+++ Leepse/Controller/SignUp/UserNameViewController.swift
@@ -10,13 +10,32 @@ import UIKit
 
 class UserNameViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func tappedSubmit(_ sender: UIButton) {
+        let phoneNumberSignUpViewController = UIStoryboard(name: "SignUp", bundle: .main).instantiateViewController(withIdentifier: "PhoneNumberSignUpVc") as! PhoneNumberSignUpViewController
+        navigationController?.pushViewController(phoneNumberSignUpViewController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configuringTheNavigationBar()
+        customLabelAndButton()
     }
     
     func configuringTheNavigationBar() {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.backBarButtonItem?.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+    
+    func customLabelAndButton() {
+        messageLabel.text = ""
+        submitButton.layer.cornerRadius = 16.7
+        submitButton.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        submitButton.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.7176470588, blue: 0.3921568627, alpha: 1)
     }
 }
