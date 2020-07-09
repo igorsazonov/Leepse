89e1eb3046bdd4ed769a3158cee627edf341b517
igorsazonov <sazonovkerch@gmail.com>
2020-07-09 07:04:20 +0300

    changes 08.08

--- Leepse/Controller/FirstScreenViewController.swift
+++ Leepse/Controller/FirstScreenViewController.swift
@@ -12,16 +12,7 @@ class FirstScreenViewController: UIViewController {
 
     @IBOutlet weak var signUpButton: UIButton!
     
-    @IBAction func signUpButtounTapped(_ sender: UIButton) {
-    
-    }
-    
     @IBOutlet weak var logInButton: UIButton!
-    
-    @IBAction func logInButtounTapped(_ sender: UIButton) {
-        let signInViewController = (UIStoryboard(name: "SignIn", bundle: Bundle.main).instantiateViewController(withIdentifier: "EnterPhoneVc") as? EnterPhoneNumberViewController)!
-        navigationController?.pushViewController(signInViewController, animated: true)
-    }
     
     @IBOutlet weak var messageHeaderLabel: UILabel!
     
@@ -29,17 +20,28 @@ class FirstScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        configuringTheNavigationBar()
         settingUpTheViewButtons()
-        messageHeaderLabel.text = "A smart forum"
-        messageLabel.text = "where you decide who can reply to your posts, and who can't"
+        configuringTheLabel()
     }
+    
+    func configuringTheNavigationBar() {
+        navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "backButton")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "backButton")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+     }
     
     func settingUpTheViewButtons() {
         signUpButton.layer.cornerRadius = 16.7
         signUpButton.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         signUpButton.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.7176470588, blue: 0.3921568627, alpha: 1)
-        
         logInButton.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+    
+    func configuringTheLabel() {
+        messageHeaderLabel.text = "A smart forum"
+        messageLabel.text = "where you decide who can reply to your posts, and who can't"
     }
 }
